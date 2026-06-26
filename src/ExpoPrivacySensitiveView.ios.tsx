4780484778505e741814869c422cc64dad078a83
Samuel Newman <mozzius@protonmail.com>
2026-06-26 12:06:48 +0300

    upgrade bunch of deps

--- src/ExpoPrivacySensitiveView.ios.tsx
+++ src/ExpoPrivacySensitiveView.ios.tsx
@@ -1,11 +1,13 @@
-import { requireNativeView } from 'expo';
-import * as React from 'react';
+import { requireNativeView } from "expo";
 
-import { PrivacySensitiveProps } from './ExpoPrivacySensitive.types';
+import { type PrivacySensitiveProps } from "./ExpoPrivacySensitive.types";
 
 const NativeView: React.ComponentType<PrivacySensitiveProps> =
-  requireNativeView('ExpoPrivacySensitive');
+  requireNativeView("ExpoPrivacySensitive");
 
-export default function PrivacySensitive({ children, style }: PrivacySensitiveProps) {
+export default function PrivacySensitive({
+  children,
+  style,
+}: PrivacySensitiveProps) {
   return <NativeView style={style}>{children}</NativeView>;
 }
